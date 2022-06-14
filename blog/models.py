3840b53283b6a8d7fef93d5e9610bb75b8b83719
Lumi <shensanlumi@gmail.com>
2022-06-14 15:11:44 +0100

    m

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    image = models.CharField(null=True, blank=True, max_length=233)
+    image = models.ImageField(null=True, blank=True, max_length=233)
 
     def __str__(self):
         return self.title
